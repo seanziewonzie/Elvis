b109848f45470dd10fcc794a0bca8f9ed7c74049
baquinn4 <bquinn6@lsu.edu>
2018-06-06 12:45:57 -0500

    added main()

--- faces.py
+++ faces.py
@@ -1,22 +1,30 @@
+class PlaneDecomp:
+	#plane decompisiton function
+	#Prompt user for number of faces.
 
-#plane decompisiton function
-#Prompt user for number of faces.
-n=(int) (input('How many faces? '))
-faces=[]
-adjgraph= []
+	global n
+	faces=[]
+	adjgraph=[]
 
-#For each face:
-for i in range(0,n):
+	def main():
+		n=(int)(input('How many faces? '))
+		#For each face:
+		for i in range(0,n):
 
-	#Get number of halfspaces which define the face.
-	m=(int) (input('How many halfspaces cut out face '+str(i+1)+'? '))
+			#Get number of halfspaces which define the face.
+			m=(int)(input('How many halfspaces cut out face '+str(i+1)+'? '))
 
-	#Determine the halfspaces which define the face.
-	hspaces=[]
-	for j in range(0,m):
-		hspacenums = input('Give a triple indicating a half-space: ').split(' ') #will take in a string of numbers separated by a space
-		hspace = [float(num) for num in hspacenums]
-		hspaces.append(hspace)
+			#Determine the halfspaces which define the face.
+			hspaces=[]
+			for j in range(0,m):
+				hspacenums = raw_input('Give a triple indicating a half-space: ').split(" ")#will take in a string of numbers separated by a space
+				hspace = [float(num) for num in hspacenums]
+				hspaces.append(hspace)
 
-	#Store array of halfspaces in face array.
-	faces.append(hspaces)
+		#Store array of halfspaces in face array.
+		faces.append(hspaces)
+		
+	if __name__ == "__main__":
+		main()
+
+		
