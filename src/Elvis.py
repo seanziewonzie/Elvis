282dade6409172b73179a1f82a4f0d5837fd842f
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-12 13:35:50 -0500

    Update and remain main.py to Elvis.py
    Now this just simply calls on spaceDecomp to get a Space Decomposition.

---
+++ src/Elvis.py
@@ -0,0 +1,10 @@
+from sage.all import *
+from SpaceDecomp import *
+class Elvis:
+	
+	def main():
+		sd=SpaceDecomp()
+		spaceDecomp=sd.SpaceDecompChoice()
+		
+	if __name__ == "__main__":
+		main()
