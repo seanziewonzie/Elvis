4dbbcf085a63a7c42c0f2fd13564f7f954756a82
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-12 13:34:15 -0500

    Update SpaceDecomp.py
    Now it asks if you want to create a new SpaceDecomp or use an existing one.

--- src/SpaceDecomp.py
+++ src/SpaceDecomp.py
@@ -1,41 +1,36 @@
-#plane decompisiton function
-#Prompt user for number of faces.
-
-#subprocess allows us to run Linux commands from the python script
-import errno
-import subprocess
-import os
 from sage.all import *
-
 class SpaceDecomp:
-	#the array of regions
-	regions=[]
-
-	#the simplified/optimized adjacency graph of the regions
-	adjgraph=[]
-
-	#variable to distinguish test session vs real use
-	global t
-	t= ""
-
-	global m
-	m=0
-	global n
-	n = 0
-	global d
-	d = 0
+	global response
+	response =""
+	global spaceDecomp
+	spaceDecomp=[]
+	def __init__(self):
+		self.response = response
+		self.spaceDecomp= spaceDecomp
 	
-	def __init__(self,d,n):
-		self.n = n
-		self.d = d
-	def getGraph(self):
 		
-		t=raw_input("Save data? y/n: ")
-		t=str(t)
-
+	def SpaceDecompChoice(self):
+		self.response =raw_input('Do you want to use a saved space decomposition? ')
+		while self.response !='y' and self.response !='n':
+			print('This is not a valid input. Please type y/n: ')
+			self.response =raw_input('Do you want to use a saved space decomposition? ')
+		if self.response == 'y':
+			print('We have not coded save and write yet')
+			#return self.chooseSpaceDecomp()
+		if self.response =='n':
+			print('You will now create a new space decomposition.')
+			return self.createSpaceDecomp()
 		
 	
-		for i in range(self.n):
+	
+	def createSpaceDecomp(self):
+		d=(int)(input('What is the dimension? '))
+		n=(int)(input('How many regions? '))
+		#the array of regions
+		regions=[]
+		#the simplified/optimized adjacency graph of the regions
+		adjarray=[]		
+		for i in range(n):
 			while True:
 				#Get number of halfspaces which define the face.
 				m=(int)(input('How many halfspaces cut out region '+str(i+1)+'? '))
@@ -44,22 +39,22 @@ class SpaceDecomp:
 				for j in range(m):
 					while True:
 						if j==0:
-							rawnums = raw_input('Give a list of ' + str(self.d + 1) + ' numbers to indicate the ' +str(j+1) +'st halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
+							rawnums = raw_input('Give a list of ' + str(d + 1) + ' numbers to indicate the ' +str(j+1) +'st halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
 						elif j==1:
-							rawnums = raw_input('Give a list of ' + str(self.d + 1) + ' numbers to indicate the ' +str(j+1) +'nd halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
+							rawnums = raw_input('Give a list of ' + str(d + 1) + ' numbers to indicate the ' +str(j+1) +'nd halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
 						elif j==2:
-							rawnums = raw_input('Give a list of ' + str(self.d + 1) + ' numbers to indicate the ' +str(j+1) +'rd halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
-					else:
-						rawnums = raw_input('Give a list of ' + str(self.d + 1) + ' numbers to indicate the ' +str(j+1) +'th halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
+							rawnums = raw_input('Give a list of ' + str(d + 1) + ' numbers to indicate the ' +str(j+1) +'rd halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
+						else:
+							rawnums = raw_input('Give a list of ' + str(d + 1) + ' numbers to indicate the ' +str(j+1) +'th halfspace, where a0 a1 ... an indicates the halfspace corresponsing to the inqeuality a0 + a1x1 + ... anxn <= 0 \n').split(" ") 
 						#will take in a string of numbers separated by a space
-					if len(rawnums) != self.d+1:
-						#This checks if the halfspace is the right dimension
-						print('This list is the wrong length. Try again.')
-					else:
-						break
+						if len(rawnums) != d+1:
+							#This checks if the halfspace is the right dimension
+							print('This list is the wrong length. Try again.')
+						else:
+							break
 					hspace = [float(num) for num in rawnums]
 					hspaces.append(hspace)
-					#The user will keep inputting the region until it does not intersect with previous regions
+				#The user will keep inputting the region until it does not intersect with previous regions
 				overlap=0
 				for j in range(0,i):
 					p=Polyhedron(ieqs=hspaces + regions[j])
@@ -82,8 +77,17 @@ class SpaceDecomp:
 						adjacency=1
 						#Check against every other adjacency and see if this new adjacency is subsumed by it, or subsumes it
 						for k in range(i):
+							if adjarray[j][k]==0:
+								adjacency=1
+								#no need to check against an adjacency which doesn't exist
 							if adjarray[j][k]==1:
 								kjPoly=Polyhedron(ieqs=regions[k]+regions[j])
+								if kjPoly&ijPoly==ijPoly:
+									adjacency=adjacency*0
+								if kjPoly&ijPoly==kjPoly:=[d,n,adjGraph]
+									adjacency=adjacency*1
+									adjarray[k][j]=0
+									adjarray[j][k]=0
 								if kjPoly!=ijPoly:
 									if kjPoly&ijPoly==ijPoly:
 										adjacency=0
@@ -92,6 +96,7 @@ class SpaceDecomp:
 									if kjPoly&ijPoly==kjPoly:
 										adjarray[k][j]=0
 										adjarray[j][k]=0
+
 					else:
 						adjacency=0
 						print('Regions ' + str(i) + ' and ' + str(j) + ' dont intersect')
@@ -100,35 +105,14 @@ class SpaceDecomp:
 				#The final entry is the adjanceny of i with itself, which we consider to be 0 for simplification 
 				adjrow.append(0)
 				adjarray.append(adjrow)
-			
-	
-	
-		for i in range(0,self.n):
-			print('This is region ' + str(i+1))
-			print(regions[i])
-		
-		for i in range(0,n):
-			guprint(adjgraph[i])
-			print('This is the adjacency graph')
-	#method to save data, will either add similar method to each class or make standalone class to import		
-	def save(self):
-		if t == "y":
-			#change working dir to /home/Documents
-			os.chdir(os.path.expanduser("~/Documents"))
-			try:
-				#try and change working dir to /home/Documents/ElvisData
-				os.chdir(os.path.expanduser("~/Documents/ElvisData"))
-			except OSError as error:
-				#if /home/Documents/ElvisData doesn't exits, create the dir in /home/Documents/
-				if error.errno == errno.ENOENT:
-					subprocess.call(["mkdir","ElvisData"]) 
-				#change working dir to /home/Documents/ElvisData          
-				os.chdir(os.path.expanduser("~/Documents/ElvisData"))
-			finally:
-				#create a file in /home/Documents/ElvisData
-				subprocess.call(["touch",str(self.d)+"-dimens_"+str(self.n)+"-regions"+".txt"])
+		keys=[]
+		for i in range(n):
+			keys.append(i)
+		regionDictionary=dict(zip(keys,regions))
+		adjmatrix=Matrix(adjarray)	
+		adjGraph=Graph(adjmatrix)
+		adjGraph.set_vertices(regionDictionary)
+		self.spaceDecomp =[d,n,adjGraph]
+		return self.spaceDecomp
 
-
-	if __name__ == "__main__":
-		getGraph()
-		save()
+	
