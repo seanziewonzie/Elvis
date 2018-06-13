a277164fb2c82dcb9a45d22eb28fcb2cdf6e160e
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-13 12:14:39 -0500

    Create StartAndEnd.py
    This will be called only when making a new calculation. The method is not written out yet, so an in-construction method takes its place.

---
+++ src/StartAndEnd.py
@@ -0,0 +1,20 @@
+from sage.all import *
+class StartAndEnd:
+	
+	def createStartAndEnd(self,spaceDecomp):
+		#Ask for start point by startCoords= raw_input('What is the starting point? \n')
+		#Check that this is the right length
+		#Check that it is actually in one of the regions. 
+		#If it is, let startRegion = that region
+		#Let start = [startCoords, startRegion]
+		
+		
+		#Ask for end point by endCoords= raw_input('What is the ending point? \n')
+		#Check that this is the right length
+		#Check that it is actually in one of the regions. 
+		#If it is, let endRegion = that region
+		#Let end = [endCoords, endRegion]
+		
+		#return [start, end]
+		
+		return 'There is not yet a way to enter start and endpoints. '
