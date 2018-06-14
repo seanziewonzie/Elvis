fe9434e94720d9fa0380d188de1d1a5ff79c79d3
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-14 13:48:40 -0500

    Update Calculations.py
    There were some errors with regard to calling other methods and classes.

--- src/Calculation.py
+++ src/Calculation.py
@@ -1,17 +1,19 @@
 from sage.all import *
 from Situation import *
+from StartAndEnd import *
+from SaveMessage import *
 class Calculation:
 	
 	#The user will be asked if they want to utilize a calculation which has previously been made or if they want to make a new one.	
 	def calculationLoadOrNew(self):
-		response =raw_input('Press l to load a calculation. Press n to create a new one. ')
+		response =raw_input('\nPress l to load a calculation. \nPress n to create a new calculation. \n')
 		while response !='l' and response !='n':
 			print('This is not a valid input. Please type l/n: ')
-			response =raw_input('Press l to load a calculation. Press n to create a new one. ')
+			response =raw_input('\nPress l to load a calculation. \n Press n to create a new calculation. \n')
 		if response == 'l':
 			return self.chooseSpaceDecomp()
 		if response =='n':
-			print('You will now create a new calculation.')
+			print('\nYou will now create a new calculation.')
 			return self.createCalculation()
 	
 	
@@ -21,7 +23,7 @@ class Calculation:
 		#plotting 2-D and maybe 3-D calculations, merging calculations, renaming calculations, and maybe more
 	
 
-	def chooseCalculation():
+	def chooseCalculation(self):
 		#We let them load a calculation from the Calculations folder, and see which Calculations are in the Calculation folder
 		#calculation = whatever the user chooses
 		#return calculation
@@ -30,28 +32,36 @@ class Calculation:
 
 
 
-	def createCalculation():
+	def createCalculation(self):
+		#get the situation
+		sit = Situation()
+		situation = sit.situationLoadOrNew()
+
+
 
 		#get start and end points within this space decomposition
-		sae = StartAndEnd
-		startAndEnd = sae.createStartAndEnd(spaceDecomp)
+		sae = StartAndEnd(situation)
+		startAndEnd = sae.createStartAndEnd()
 		
 		#With these three specifications, the optimal path problem is well defined
 		input = [situation,startAndEnd]
 		
-		calculation = self.optimization(input)
+		calculation = self.Optimization(input)
 		
-		saveOption = raw_input('Enter s to save this calculation. Enter anything else to move on. ')
-		if saveOption == 's':
-			print('Saving calculations is not a feature yet. ')
+		self.saveCalculation()
 			
 		return calculation
 
 
-	def Optimization(input):
+	def Optimization(self,input):
 		#This classifies and enumerates all paths in our space decomposition from the start point to the end point
 		#In fact, it only classifies the ones worthy of consideration
 		paths = input[0][0][2].all_paths(input[1][0][1],input[1][1][1])
+		p = len(paths)
+		if p == 0:
+			print ('There are no paths between the starting point and the ending point.\n')
+		for i in range(p):
+			print('Calculus goes here. ')
 		
 		#for each path in the array paths, the next bit will define a function based off the velocity sets and number of paths
 		#Then it will optimize the function (constrained to only choose boundary points between the regions in the path) 
@@ -66,3 +76,7 @@ class Calculation:
 		#return optimalAnswer
 		
 		return 'We have not yet coded a way to find an optimal path'
+
+	def saveCalculation(self):
+		sm = SaveMessage('calculation')
+		sm.message()
