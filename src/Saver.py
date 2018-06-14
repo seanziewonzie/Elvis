8360d556f4d4bf4bf634c39ed81e38d8a7b6163c
baquinn4 <bquinn6@lsu.edu>
2018-06-14 11:42:55 -0500

    Update structure

--- src/Saver.py
+++ src/Saver.py
@@ -7,26 +7,25 @@ def main():
 	print platform.system()
 
 def Construct():
+
 	yourOS = platform.system()
 	if yourOS == "Linux":
-		os.chdir(os.path.expanduser("~/Documents/"))
-		subprocess.call(["mkdir","Elvis"])
-		os.chdir(os.path.expanduser("~/Documents/Elvis"))
 		subprocess.call(["mkdir","Calculations"])
 		subprocess.call(["mkdir","Situations"])
 		os.chdir(os.path.expanduser("~/Documents/Elvis/Situations"))
-		subprocess.call(["mkdir","SpaceDecomp"])
+		subprocess.call(["mkdir","SpaceDecompExampleFolder"])
 	if yourOS == "Windows":
 		subprocess.call("start","cmd")
 		os.chdir(os.path.expanduser("%userprofile%"))
-		subprocess.call(["mkdir","Elvis"])
 		os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis"))
 		subprocess.call(["mkdir","Calculations"])
 		subprocess.call(["mkdir","Situations"])
 		os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis\Situations"))
-		subprocess.call(["mkdir","SpaceDecomp"])
+		subprocess.call(["mkdir","SpaceDecompExampleFolder"])
 
-def Save():
+def Save(object):
+	
+
 
 
 if __name__ == "__main__":
