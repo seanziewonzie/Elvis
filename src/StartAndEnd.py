87aac4d52661129f751510dcc6dd5f63ef8cabcd
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-14 13:52:11 -0500

    Update StartAndEnd.py
    It actually does something now.

--- src/StartAndEnd.py
+++ src/StartAndEnd.py
@@ -1,20 +1,35 @@
 from sage.all import *
 class StartAndEnd:
-	
-	def createStartAndEnd(self,spaceDecomp):
-		#Ask for start point by startCoords= raw_input('What is the starting point? \n')
-		#Check that this is the right length
-		#Check that it is actually in one of the regions. 
-		#If it is, let startRegion = that region
-		#Let start = [startCoords, startRegion]
-		
-		
-		#Ask for end point by endCoords= raw_input('What is the ending point? \n')
-		#Check that this is the right length
-		#Check that it is actually in one of the regions. 
-		#If it is, let endRegion = that region
-		#Let end = [endCoords, endRegion]
-		
-		#return [start, end]
-		
-		return 'There is not yet a way to enter start and endpoints. '
+	def __init__(self,situation):
+		self.situation = situation
+
+	def createStartAndEnd(self):
+		start = self.pointInfo("start")
+		end = self.pointInfo("end")
+		return [start,end]
+
+	def pointInfo(self,string):
+		spaceDecomp = self.situation[0]
+		d = spaceDecomp[0]
+		n = spaceDecomp[1]
+		adjGraph = spaceDecomp[2]
+
+		while True:
+			contained = 0
+			rawCoords=raw_input('\nWhat are the coordinates of the ' +string +'ing point? \n').split(" ")
+			if len(rawCoords)!=d:
+				print('The '+ string +'ing point should be specified by ' + str(d) + 'coordinates. Try again. ')
+			else:
+				coords = [float(num) for num in rawCoords]
+				for i in  range(n):
+					P = adjGraph.get_vertex(i)
+					if P.contains(coords):
+						contained = 1
+						region = i 
+						break
+			if contained ==1:
+				break
+			else:
+				print('This point is not contained in any region. Try again. ')
+
+		return [coords,region]
