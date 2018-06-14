e65657729ac5d8cae5de9a11cd8cf5973007b088
baquinn4 <bquinn6@lsu.edu>
2018-06-13 20:35:32 -0500

    Save methods for Windows/Linux os

---
+++ src/Saver.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import platform
+
+def main():
+	Construct()
+	print platform.system()
+
+def Construct():
+	yourOS = platform.system()
+	if yourOS == "Linux":
+		os.chdir(os.path.expanduser("~/Documents/"))
+		subprocess.call(["mkdir","Elvis"])
+		os.chdir(os.path.expanduser("~/Documents/Elvis"))
+		subprocess.call(["mkdir","Calculations"])
+		subprocess.call(["mkdir","Situations"])
+		os.chdir(os.path.expanduser("~/Documents/Elvis/Situations"))
+		subprocess.call(["mkdir","SpaceDecomp"])
+	if yourOS == "Windows":
+		subprocess.call("start","cmd")
+		os.chdir(os.path.expanduser("%userprofile%"))
+		subprocess.call(["mkdir","Elvis"])
+		os.chdir(os.path.expanduser("%userprofile%/Documents/Elvis"))
+		subprocess.call(["mkdir","Calculations"])
+		subprocess.call(["mkdir","Situations"])
+		os.chdir(os.path.expanduser("%userprofile%/Documents/Elvis/Situations"))
+		subprocess.call(["mkdir","SpaceDecomp"])
+
+#def Save():
+
+
+if __name__ == "__main__":
+	main()
+	
