f12e3faf50d0183e85b11388f41059e0cc14ced7
guido3245 <37053938+guido3245@users.noreply.github.com>
2018-06-15 11:54:58 -0500

    Add files via upload

--- Setup.py
+++ Setup.py
@@ -1,27 +1,37 @@
-import sys; sys.dont_write_bytecode = True
 import os
 import subprocess
 import platform
+
 def main():
 	Construct()
 	print platform.system()
 
-
 def Construct():
+
 	yourOS = platform.system()
 	if yourOS == "Linux":
 		subprocess.call(["mkdir","Calculations"])
 		subprocess.call(["mkdir","Situations"])
 		os.chdir(os.path.expanduser("~/Documents/Elvis/Situations"))
 		subprocess.call(["mkdir","SpaceDecompExampleFolder"])
+
 	if yourOS == "Windows":
-		subprocess.call("start","cmd")
-		os.chdir(os.path.expanduser("%userprofile%"))
-		os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis"))
-		subprocess.call(["mkdir","Calculations"])
-		subprocess.call(["mkdir","Situations"])
-		os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis\Situations"))
-		subprocess.call(["mkdir","SpaceDecompExampleFolder"])
+		os.mkdir("Calculations")
+		os.mkdir("Situations")
+		os.chdir("Situations")
+		os.mkdir("SpaceDecompExampleFolder")
+		#os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis"))
+		#os.system("mkdir Calculations")
+		#os.system("mkdir Situations")
+		#subprocess.call(["mkdir","Calculations"])
+		#subprocess.call(["mkdir","Situations"])
+		#os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis\Situations"))
+		#subprocess.call(["mkdir","SpaceDecompExampleFolder"])
+
+	
+
+
 
 if __name__ == "__main__":
-	main()	
+	main()
+	
