9b04556d774b2e46ef95cf5d0cc41b544a3280fb
guido3245 <37053938+guido3245@users.noreply.github.com>
2018-06-15 12:38:01 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,30 +7,10 @@ def main():
 	print platform.system()
 
 def Construct():
-
-	yourOS = platform.system()
-	if yourOS == "Linux":
-		subprocess.call(["mkdir","Calculations"])
-		subprocess.call(["mkdir","Situations"])
-		os.chdir(os.path.expanduser("~/Documents/Elvis/Situations"))
-		subprocess.call(["mkdir","SpaceDecompExampleFolder"])
-
-	if yourOS == "Windows":
-		os.mkdir("Calculations")
-		os.mkdir("Situations")
-		os.chdir("Situations")
-		os.mkdir("SpaceDecompExampleFolder")
-		#os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis"))
-		#os.system("mkdir Calculations")
-		#os.system("mkdir Situations")
-		#subprocess.call(["mkdir","Calculations"])
-		#subprocess.call(["mkdir","Situations"])
-		#os.chdir(os.path.expanduser("%userprofile%\Documents\Elvis\Situations"))
-		#subprocess.call(["mkdir","SpaceDecompExampleFolder"])
-
-	
-
-
+	os.mkdir("Calculations")
+	os.mkdir("Situations")
+	os.chdir("Situations")
+	os.mkdir("SpaceDecompExampleFolder")
 
 if __name__ == "__main__":
 	main()
