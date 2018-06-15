1abce4584936ee170f3a8f6894c9996f541ef30b
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-14 20:20:54 -0500

    Update Calculation.py
    The ending lines of handleCalculation have been written.

--- src/Calculation.py
+++ src/Calculation.py
@@ -2,25 +2,29 @@ from sage.all import *
 from Situation import *
 from StartAndEnd import *
 from SaveMessage import *
+from ErrorMessage import errorMessage
 class Calculation:
 	
 	#The user will be asked if they want to utilize a calculation which has previously been made or if they want to make a new one.	
 	def calculationLoadOrNew(self):
-		response =raw_input('\nPress l to load a calculation. \nPress n to create a new calculation. \n')
-		while response !='l' and response !='n':
-			print('This is not a valid input. Please type l/n: ')
-			response =raw_input('\nPress l to load a calculation. \n Press n to create a new calculation. \n')
-		if response == 'l':
+		response =raw_input('\nPress 1 to load a calculation. \nPress 2 to create a new calculation. \n')
+		while response != 1 and response !=2:
+			response =raw_input('Please enter 1 or 2. \nPress 1 to load a calculation. \nPress 2 to create a new calculation. \n')
+		if response == '1':
 			return self.chooseSpaceDecomp()
-		if response =='n':
+		if response =='2':
 			print('\nYou will now create a new calculation.')
 			return self.createCalculation()
-	
+
 	
 	def handleCalculation(self,calculation):
-		return 'We have not coded any options to handle calculations'
+		print('We have not coded any options to handle calculations')
+
 		#Here they will have the option of deleting calculations, getting a text description of a calculation, 
-		#plotting 2-D and maybe 3-D calculations, merging calculations, renaming calculations, and maybe more
+		#plotting 2-D and maybe 3-D calculations, merging calculations, renaming calculations, and maybe more.
+
+		response = raw_input('\nEnter 1 to perform another action with this calculation. \nEnter 2 to get go back to the beginning. \n')
+		return response
 	
 
 	def chooseCalculation(self):
@@ -31,21 +35,18 @@ class Calculation:
 		
 
 
-
+	#The user will create a calculation from a situation and a choice of start and end points.
 	def createCalculation(self):
-		#get the situation
+		#Get the situation.
 		sit = Situation()
 		situation = sit.situationLoadOrNew()
 
-
-
-		#get start and end points within this space decomposition
+		#Get the start and end points within this space decomposition.
 		sae = StartAndEnd(situation)
 		startAndEnd = sae.createStartAndEnd()
 		
-		#With these three specifications, the optimal path problem is well defined
+		#With these three specifications, the time function is well defined
 		input = [situation,startAndEnd]
-		
 		calculation = self.Optimization(input)
 		
 		self.saveCalculation()
@@ -53,16 +54,19 @@ class Calculation:
 		return calculation
 
 
+	#Given an input, this method will find the path in the situation from the start point to the endpoint which
+	#minimizes the time function.
 	def Optimization(self,input):
 		#This classifies and enumerates all paths in our space decomposition from the start point to the end point
 		#In fact, it only classifies the ones worthy of consideration
 		paths = input[0][0][2].all_paths(input[1][0][1],input[1][1][1])
 		p = len(paths)
 		if p == 0:
-			print ('There are no paths between the starting point and the ending point.\n')
+			return '\nThere are no paths between the starting point and the ending point.\n'
 		for i in range(p):
-			print('Calculus goes here. ')
-		
+			return '\nWe have not yet coded a way to find an optimal path. \n'
+			#Calculus goes here
+
 		#for each path in the array paths, the next bit will define a function based off the velocity sets and number of paths
 		#Then it will optimize the function (constrained to only choose boundary points between the regions in the path) 
 		#Specifically, it will find the sequence of boundary points which causes the best time, and the time in particular
@@ -74,8 +78,6 @@ class Calculation:
 		#optimalAnswer will be the duple with the best time
 		
 		#return optimalAnswer
-		
-		return 'We have not yet coded a way to find an optimal path'
 
 	def saveCalculation(self):
 		sm = SaveMessage('calculation')
