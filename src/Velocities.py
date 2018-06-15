81b14de5aa712020878cc18ca94e9a769945602b
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-14 20:26:56 -0500

    Update Velocities.py
    Some comments. Added a try/except.

--- src/Velocities.py
+++ src/Velocities.py
@@ -1,25 +1,25 @@
 from sage.all import *
 from Calculation import *
 from SaveMessage import *
+from ErrorMessage import errorMessage
 class Velocities:
 	def __init__(self,spaceDecomp):
 		self.spaceDecomp = spaceDecomp
 	
 	def velocitiesLoadOrNew(self):
 		#if there are some velocity sets associated with this spaceDecomp, run this
-		self.response =raw_input('\nPress l to load a velocity set associated with this space decompostion. \nPress n to create a new velocity set associated with this space decompostion. ')
-		while self.response !='l' and self.response !='n':
-			print('This is not a valid input. Please type l/n: ')
-			self.response =raw_input('\nPress l to load a velocity set associated with this space decomposition. \nPress n to create a new velocity set associated with this space decompostion. ')
-		if self.response == 'l':
-			return self.chooseVecloties()
-		if self.response =='n':
-			print('You will now create a new velocity set for this space decomposition.')
-			return self.createVelocities()
+			#self.response =raw_input('\nPress 1 to load a velocity set associated with this space decompostion. \nPress 2 to create a new velocity set associated with this space decompostion. ')
+			#while response != 1 and response !=2
+				#self.response =raw_input('Please enter 1 or 2. \nPress 1 to load a velocity set associated with this space decompostion. \nPress 2 to create a new velocity set associated with this space decompostion. ')
+			#if self.response == '1':
+				#return self.chooseVelocities()
+			#if self.response =='2':
+				#print('You will now create a new velocity set for this space decomposition.')
+				#return self.createVelocities()
 
 		#if there are no velocity sets associated with this spaceDecomp, run this
-			#print (There are no velocity sets associated with this space decomposition. You will now create one. ')
-			#return self.createVelocities
+		print('There are no velocity sets associated with this space decomposition. You will now create one. ')
+		return self.createVelocities()
 		
 		
 		
@@ -30,15 +30,18 @@ class Velocities:
 		
 		return 'We have not coded the saving and loading of velocity sets yet.' 
 		
-	def createVelocities(self):
-		n = self.spaceDecomp[1]
 
+	#This method will create a new velocity set for this space decomposition.
+	def createVelocities(self):
+		#There will be exactly one velocity set associated to each region in the space decomposition.
+		n = self.spaceDecomp[1]
 		velocities = []
 		for i in range(n):
 			while True:
-				#For now, velocity sets are just sphere, defined by a real number. This method will have to be rewritten to accept 
-				#any function in hyperspherical coordinates.
+				#The user will be prompted to enter a valid velocity set.
 				try:
+					#For now, velocity sets are just sphere, defined by a real number. This method will have to be rewritten to accept 
+					#any function in hyperspherical coordinates.
 					input = (float)(raw_input('\nEnter a positive real number to indicate the velocity associated with region ' +str(i+1) +'. \n'))
 					if input > 0:
 						break
@@ -49,11 +52,12 @@ class Velocities:
 			velocities.append(input)
 
 		self.saveVelocitySet()
+
 		return velocities
+
 
 	def saveVelocitySet(self):
 		sm = SaveMessage('velocity set')
 		sm.message()
 
-		#Option to save this velocity set under the folder for this spaceDecomp
 		
