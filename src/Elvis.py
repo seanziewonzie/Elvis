a4905fc293e80a77ce4fca24c1cfe4d2002e4f6f
seanziewonzie <35290945+seanziewonzie@users.noreply.github.com>
2018-06-22 16:23:37 -0500

    Update Elvis.py

--- src/Elvis.py
+++ src/Elvis.py
@@ -1,11 +1,11 @@
 import sys; sys.dont_write_bytecode = True
 from sage.all import *
-from Calculation import *
-from Situation import *
+import CalculationsAndSituations as CAS
 import Message
 #In Elvis, there are two kinds of objects which are stored on the memory drive: situations and calculations.
 #This method will ask the user which kind of object they wish to deal with.
-def firstChoice(self):
+def firstChoice():
+	#Keep looping the program from the beginning until the user quits somewhere.
 	while True:
 		response = Message.getResponse('\nEnter 1 to get a calculation. \nEnter 2 to get a situation. \n')
 		if response != '1' and response != '2':
@@ -13,23 +13,23 @@ def firstChoice(self):
 			continue
 		#Get a calculation, and handle it until the user asks to return to the beginning.
 		if response == '1':
-			calc=Calculation()
-			calculation=calc.calculationLoadOrNew()
-			calc.handleCalculation(calculation)
+			calc=CAS.Calculation()
+			calc.calculationLoadOrNew()
+			calc.handleCalculation()
 		#Get a situation, and either view or edit it until the user asks to return to the beginning.
 		if response =='2':
-			sit=Situation()
-			situation=sit.situationLoadOrNew()
-			sit.viewOrEditSituation(situation)
+			sit=CAS.Situation()
+			sit.situationLoadOrNew()
+			sit.viewOrEditSituation()
 
 
-def welcomeMessage(self):
+def welcomeMessage():
 	print('\n      ------------------------------\n       Thank you for using Elvis. \n      Enter q at any time to quit.\n      ------------------------------\n')
 
 
-def main(self):
-	self.welcomeMessage()
-	self.firstChoice()
+def main():
+	welcomeMessage()
+	firstChoice()
 
 if __name__ == "__main__":
 	main()
