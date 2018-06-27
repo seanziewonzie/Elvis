f26c0abb4b7216eac73083eb7956a1a735a58777
guido3245 <37053938+guido3245@users.noreply.github.com>
2018-06-27 12:51:04 -0500

    Add files via upload

---
+++ dependencies/elvisHelper.bat
@@ -0,0 +1,4 @@
+@echo off
+move .\Downloads\Elvis-master\Elvis-master\dependencies\SageMath C:\"Program Files"\Elvis-master
+start .\Downloads\Elvis-master\Elvis-master\dependencies\python-2.7.12.amd64.msi
+exit
